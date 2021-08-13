58f5f3aa4e71518f81367319d47dc7e0553b2b20
Joyce Lee <joycelee901@gmail.com>
2021-08-13 12:02:20 -0500

    updated cors files to allow different port request

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             render json: user, status: :created
         else
+            byebug
             render json: {errors: ["Invalid username or password"]}, status: :unauthorized
         end
     end
