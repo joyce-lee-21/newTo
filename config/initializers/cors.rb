c17577c29a2ac773f618979751af5f4425904a40
Joyce Lee <joycelee901@gmail.com>
2021-10-06 12:54:36 -0500

    updated cors url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://newto-backend.herokuapp.com'
+    origins ['http://localhost:3001', 'https://new-to.netlify.app']
 
     resource '*',
       headers: :any,
